d1af4b24b07880d9aec33ba37fe99bfc23e43482
Silver6819 <mr6874823@gmail.com>
2026-04-29 13:53:27 -0600

    enemigos.py

--- enemigos.py
+++ enemigos.py
@@ -1,1 +1,6 @@
-
+BASE_DE_DATOS_ENEMIGOS = [
+    {"nombre": "Espectro Menor", "hp": 70, "atk": 12, "desc": "Una sombra del vacío."},
+    {"nombre": "Guerrero Shura", "hp": 160, "atk": 22, "desc": "Un guerrero feroz de otra época."},
+    {"nombre": "Súbdito del Culto", "hp": 110, "atk": 18, "desc": "Seguidor de la profecía."},
+    {"nombre": "Rey Demonio Sanguinario", "hp": 400, "atk": 45, "desc": "Una versión oscura del destino."}
+]
